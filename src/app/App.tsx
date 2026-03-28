a3dea97504e30812aed994da7db8c5a762dc5501
Roberto <roberto.larios@proton.me>
2026-03-28 17:22:41 -0600

    Fix: Instalación de react-router-dom

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// Corregimos las rutas: ./ significa "aquí mismo en la carpeta app"
+
 import { Header } from './components/Header'; 
 import { Hero } from './components/Hero';
 import { About } from './components/About';
-import { Services } from './components/Services';
 import { Contact } from './components/Contact';
+import { Projects } from './components/Projects'; 
 import { CoursesListPage } from './pages/CoursesListPage';
 
 function App() {
@@ -16,7 +16,7 @@ function App() {
           <div className="bg-black min-h-screen text-white">
             <Hero />
             <div id="about"><About /></div>
-            <div id="services"><Services /></div>
+            <div id="services"><Projects /></div> 
             <div id="contact"><Contact /></div>
           </div>
         } />
