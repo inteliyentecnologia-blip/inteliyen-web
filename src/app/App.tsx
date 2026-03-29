ead04f3de1ca62afb7b6b0bb2c075954f0d5351b
Roberto <roberto.larios@proton.me>
2026-03-28 19:39:40 -0600

    Fix: Agregada la ruta dinámica de CourseDetailPage y URL en español

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-// Componentes principales
 import { Header } from './components/Header'; 
 import { Hero } from './components/Hero';
 import { About } from './components/About';
 import { Contact } from './components/Contact';
 import { Projects } from './components/Projects'; 
-
-// Páginas de Cursos
 import { CoursesListPage } from './pages/CoursesListPage';
-// ¡AQUÍ ESTÁ LA PIEZA QUE FALTABA PARA VER LA INFORMACIÓN!
-import { CourseDetailPage } from './pages/CourseDetailPage'; 
 
 function App() {
   return (
     <Router>
       <Header /> 
       <Routes>
-        {/* Ruta principal */}
         <Route path="/" element={
           <div className="bg-black min-h-screen text-white">
             <Hero />
@@ -26,12 +20,7 @@ function App() {
             <div id="contact"><Contact /></div>
           </div>
         } />
-        
-        {/* Catálogo de Cursos (Cambiado a /cursos en español) */}
-        <Route path="/cursos" element={<CoursesListPage />} />
-        
-        {/* Detalle de cada curso dinámico (El :id es la magia para "ia-empresas") */}
-        <Route path="/cursos/:id" element={<CourseDetailPage />} />
+        <Route path="/courses" element={<CoursesListPage />} />
       </Routes>
     </Router>
   );
