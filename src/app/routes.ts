1a6418432e4124e9c64f72b90e1b1eab726fbeb6
Roberto <roberto.larios@proton.me>
2026-04-03 16:02:40 -0600

    feat: ajusta tamaño de titulo inteliyen y agrega info pequeña

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,7 +1,7 @@
-import { createBrowserRouter } from "react-router";
-import { HomePage } from "./pages/HomePage";
-import { CoursesListPage } from "./pages/CoursesListPage";
-import { CourseDetailPage } from "./pages/CourseDetailPage";
+import { createBrowserRouter } from "react-router-dom"; 
+import { HomePage } from "./pages/HomePage"; // <-- Este va CON llaves
+import CoursesListPage from "./pages/CoursesListPage"; // <-- Este va SIN llaves
+import CourseDetailPage from "./pages/CourseDetailPage"; // <-- Este va SIN llaves
 
 export const router = createBrowserRouter([
   {
